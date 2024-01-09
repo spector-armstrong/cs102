e6c187a64a66e4764559a70d3099d9c492e3ddd3
spector-armstrong <franklinluvv@gmail.com>
2024-01-09 17:54:01 +0300

    hello

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -9,7 +9,28 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    a_a = 0
+    while len(plaintext) > len(keyword):
+        keyword += keyword[a_a]
+        a_a += 1
+    for i, _ in enumerate(keyword):
+        if keyword[i].isupper():
+            key = ord(keyword[i]) - 65
+            # 65-й элемент - A. А - сдвиг на 0.
+            # Если keyword[i] - A, то 65 - 65 = 0, сдвиг на 0
+        elif keyword[i].islower():
+            key = ord(keyword[i]) - 97
+            # 97-й элемент - а.
+        if plaintext[i].isalpha():
+            c_c = ord(plaintext[i])
+            if plaintext[i].isupper() and c_c >= 91 - key:
+                ciphertext += chr(c_c - 26 + key)
+            elif plaintext[i].islower() and c_c >= 123 - key:
+                ciphertext += chr(c_c - 26 + key)
+            else:
+                ciphertext += chr(c_c + key)
+        else:
+            ciphertext += plaintext[i]
     return ciphertext
 
 
@@ -24,5 +45,23 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    a_b = 0
+    while len(ciphertext) > len(keyword):
+        keyword += keyword[a_b]
+        a_b += 1
+    for i, _ in enumerate(keyword):
+        if keyword[i].isupper():
+            key = ord(keyword[i]) - 65
+        elif keyword[i].islower():
+            key = ord(keyword[i]) - 97
+        if ciphertext[i].isalpha():
+            c_b = ord(ciphertext[i])
+            if ciphertext[i].isupper() and c_b <= 64 + key:
+                plaintext += chr(c_b + 26 - key)
+            elif ciphertext[i].islower() and c_b <= 96 + key:
+                plaintext += chr(c_b + 26 - key)
+            else:
+                plaintext += chr(c_b - key)
+        else:
+            plaintext += ciphertext[i]
     return plaintext
